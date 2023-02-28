397b598f7b19ae4dd12886e986cc0c30accd3733
Ivan Andronik <97528704+ivanka-rep@users.noreply.github.com>
2023-02-28 10:46:49 +0200

    Added cinemachine, scope fixes

---
+++ Assets/_Forest Hero_/Scripts/Utilities/FollowCamera.cs
@@ -0,0 +1,32 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace ForestHero.Utilities
+{
+	public class FollowCamera : MonoBehaviour
+	{
+		[SerializeField] private CinemachineVirtualCamera followCamera;
+		[SerializeField] private Transform target;
+
+		private void Start()
+		{
+			if (target)
+				SetNewTarget(target);
+		}
+
+		public Camera Camera { get; private set; }
+
+		public Vector3 PositionOffset => followCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset;
+
+		private void Awake()
+		{
+			Camera = GetComponentInChildren<Camera>();
+		}
+
+		public void SetNewTarget(Transform player)
+		{
+			followCamera.transform.position = player.position + PositionOffset;
+			followCamera.Follow = player;
+		}
+	}
+}
