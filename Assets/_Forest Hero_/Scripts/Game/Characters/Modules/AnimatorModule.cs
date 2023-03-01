a4db6b54c5a8c6b9060b3682943f06c9a3dbb966
Ivan Andronik <97528704+ivanka-rep@users.noreply.github.com>
2023-03-01 11:13:50 +0200

    Fixed movement animation

--- Assets/_Forest Hero_/Scripts/Game/Characters/Modules/AnimatorModule.cs
+++ Assets/_Forest Hero_/Scripts/Game/Characters/Modules/AnimatorModule.cs
@@ -1,13 +1,14 @@
-﻿using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 using Zenject;
 
 namespace ForestHero.Game.Characters.Modules
 {
 	public class AnimatorModule : MonoBehaviour
 	{
-		private static readonly int START_MOVE = Animator.StringToHash("startMove");
-		private static readonly int CANCEL_MOVE = Animator.StringToHash("cancelMove");
+		private const float MOVEMENT_CHANGE_SPEED = 15f;
+		
+		private static readonly int MOVE_MAGNITUDE = Animator.StringToHash("MoveMagnitude");
 		private static readonly int ON_GROUND = Animator.StringToHash("onGround");
 
 		[SerializeField] private Animator animator;
@@ -16,6 +17,10 @@ namespace ForestHero.Game.Characters.Modules
 		private MovementModule _movementModule;
 		private PlayerInputActions _playerInputActions;
 		private Character _character;
+		
+		private bool _isMoving;
+		private float _movementMagnitude;
+		private Vector2 _moveAxis;
 
 		[Inject]
 		private void Construct(Character character)
@@ -24,21 +29,15 @@ namespace ForestHero.Game.Characters.Modules
 			_playerInputActions = character.InputActions;
 		}
 
-		private void Awake()
-		{
-			_playerInputActions.Player.Movement.started += OnMovementStarted;
-			_playerInputActions.Player.Movement.canceled += OnMovementCanceled;
-		}
-
 		private void Update()
 		{
+			_moveAxis = _playerInputActions.Player.Movement.ReadValue<Vector2>();
+			_movementMagnitude = Mathf.Lerp(_movementMagnitude, Mathf.Min(_moveAxis.magnitude, 1f), Time.deltaTime * MOVEMENT_CHANGE_SPEED);
+
+			animator.SetFloat(MOVE_MAGNITUDE, _movementMagnitude);
+			
 			animator.SetBool(ON_GROUND, _character.MovementModule.OnGround);
 		}
-
-		private void OnMovementStarted(InputAction.CallbackContext context) => 
-			animator.SetTrigger(START_MOVE);
 		
-		private void OnMovementCanceled(InputAction.CallbackContext context) => 
-			animator.SetTrigger(CANCEL_MOVE);
 	}
 }
